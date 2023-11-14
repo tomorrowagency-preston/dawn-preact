7dfa78117d2e9e2c6b6e563ac762378fd6a1949b
Preston Roberts <preston@tomorrowagency.com>
2023-11-13 22:06:16 -0700

    reconfiguring and testing

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,8 @@
-console.log('---INDEX');
+import { h, render } from "preact";
+
+const HeadingTest = <h1>Hello, World!</h1>;
+
+const element = document.getElementById('heading_test_container');
+
+// Inject our app into the DOM
+render(HeadingTest, element);
